1b5c862c8c3ea2d400582eeda68830f024964594
Maciej Ratajski <maciejratajski1999@gmail.com>
2022-12-11 20:24:25 +0100

    better solution for updating tail in development

--- 9/main.py
+++ 9/main.py
@@ -7,11 +7,45 @@ with open('input2.txt', 'r') as file:
         step = line.split()
         step = step[0], int(step[1])
         steps.append(step)
-    length = 1
+    length = 9
     rope = Node(length)
     visited = []
     for direction, times in steps:
         for _i in range(times):
             rope.step(direction)
-            visited.append(rope.__tail)
-            print(rope.pos())
+            tail = rope.tail
+            for j in range(length-1):
+                tail = tail.tail
+            visited.append((tail[0], tail[1]))
+            snake = rope.get_snake()
+            x_min, x_max = 0,5
+            x_range = x_max - x_min + 1
+            y_min, y_max = -4, 0
+            y_range = y_max - y_min + 1
+            grid = [[str('.') for _j in range(x_range)] for _i in range(y_range)]
+            # print(grid)
+            # print(y_min, x_min)
+            for place in visited:
+                # print(place)
+                grid[place[0] - y_min][place[1] - x_min] = "#"
+            for i in range(len(snake)):
+                pos = snake[i]
+                grid[pos[0] - y_min][pos[1] - x_min] = str(i)
+            print(direction, times)
+            for row in grid:
+                print(''.join(row))
+print(len(set(visited)))
+visited = set(visited)
+# xs, ys = [grid[1] for grid in visited], [grid[0] for grid in visited]
+# x_min, x_max = min(xs), max(xs)
+# x_range = x_max - x_min + 1
+# y_min, y_max = min(ys), max(ys)
+# y_range = y_max - y_min + 1
+# grid = [[str('.') for _j in range(x_range)] for _i in range(y_range)]
+# # print(grid)
+# # print(y_min, x_min)
+# for place in visited:
+#     # print(place)
+#     grid[place[0] - y_min][place[1] - x_min] = "#"
+# for row in grid:
+#     print(''.join(row))
--- 9/node.py
+++ 9/node.py
@@ -10,9 +10,6 @@ class Node:
     def pos(self):
         return self.__pos
 
-    # def tail(self):
-    #     return self.__tail.pos()
-
     def __getitem__(self, item):
         return self.pos()[item]
 
@@ -25,26 +22,48 @@ class Node:
             self.__pos = self.__pos[0] + 1, self.__pos[1]
         elif direction == 'R':
             self.__pos = self.__pos[0], self.__pos[1] + 1
-        self.__update_tail()
+        if self.tail:
+            self.__update_tail()
 
     def __update_tail(self):
         if self.__pos[0] - self.tail[0] > 1:
-            self.tail.__pos = self.tail[0] + 1, self.tail[1]
+            self.tail.step('D')
             self.__straighten_tail('x')
         elif self.tail[0] - self.__pos[0] > 1:
-            self.tail.__pos = self.tail[0] - 1, self.tail[1]
+            self.tail.step('U')
             self.__straighten_tail('x')
         elif self.__pos[1] - self.tail[1] > 1:
-            self.tail.__pos = self.tail[0], self.tail[1] + 1
+            self.tail.step('R')
             self.__straighten_tail('y')
         elif self.tail[1] - self.__pos[1] > 1:
+            self.tail.step('L')
             self.__straighten_tail('y')
-            self.tail.__pos = self.tail[0], self.tail[1] - 1
+
+#TODO: here is the error
+    # def __straighten_tail(self, axis):
+    #     if axis == 'x':
+    #         if self.tail[1] != self.__pos[1]:
+    #             self.tail.__pos = self.tail[0], self.__pos[1]
+    #     elif axis == 'y':
+    #         if self.tail[0] != self.__pos[0]:
+    #             self.tail.__pos = self.__pos[0], self.tail[1]
 
     def __straighten_tail(self, axis):
         if axis == 'x':
-            if self.tail[1] != self.__pos[1]:
-                self.tail.__pos = self.tail[0], self.__pos[1]
+            if self.tail[1] > self.__pos[1]:
+                self.tail.step('L')
+            elif self.tail[1] < self.__pos[1]:
+                self.tail.step('R')
         elif axis == 'y':
-            if self.tail[0] != self.__pos[0]:
-                self.tail.__pos = self.__pos[0], self.tail[1]
+            if self.tail[0] > self.__pos[0]:
+                self.tail.step('U')
+            if self.tail[0] < self.__pos[0]:
+                self.tail.step('D')
+
+    def get_snake(self):
+        pos = [self.__pos]
+        tail = self.tail
+        while tail:
+            pos.append(tail)
+            tail = tail.tail
+        return pos
