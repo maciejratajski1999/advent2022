07ba9f2200d40df8e4e5f723db9f3b5d22a42ddd
Maciej Ratajski <maciejratajski1999@gmail.com>
2022-12-13 14:05:07 +0100

    the solution bug out at turnarounds because the tail updates between head jumps

--- 9/main.py
+++ 9/main.py
@@ -1,5 +1,10 @@
 from node import Node
 
+directions = {'U': (-1, 0),
+              'D': (1, 0),
+              'L': (0, -1),
+              'R': (0, 1)}
+
 with open('input2.txt', 'r') as file:
     steps = []
     for line in file.readlines():
@@ -11,29 +16,31 @@ with open('input2.txt', 'r') as file:
     rope = Node(length)
     visited = []
     for direction, times in steps:
+        print(direction, times)
         for _i in range(times):
-            rope.step(direction)
+            y, x = directions[direction]
+            rope.step(y, x)
             tail = rope.tail
             for j in range(length-1):
                 tail = tail.tail
             visited.append((tail[0], tail[1]))
             snake = rope.get_snake()
-            x_min, x_max = 0,5
+            x_min, x_max = -0,5
             x_range = x_max - x_min + 1
-            y_min, y_max = -4, 0
+            y_min, y_max = -5,0
             y_range = y_max - y_min + 1
             grid = [[str('.') for _j in range(x_range)] for _i in range(y_range)]
             # print(grid)
             # print(y_min, x_min)
             for place in visited:
-                # print(place)
+            #     # print(place)
                 grid[place[0] - y_min][place[1] - x_min] = "#"
             for i in range(len(snake)):
-                pos = snake[i]
-                grid[pos[0] - y_min][pos[1] - x_min] = str(i)
-            print(direction, times)
+                pos = snake[len(snake) - i - 1]
+                grid[pos[0] - y_min][pos[1] - x_min] = str(len(snake) - i - 1)
             for row in grid:
                 print(''.join(row))
+
 print(len(set(visited)))
 visited = set(visited)
 # xs, ys = [grid[1] for grid in visited], [grid[0] for grid in visited]
--- 9/node.py
+++ 9/node.py
@@ -10,64 +10,40 @@ class Node:
     def pos(self):
         return self.__pos
 
+    # def __directions(self, direction):
+    #
+    #     return directions[direction]
+
     def __getitem__(self, item):
         return self.pos()[item]
 
-    def step(self, direction):
-        if direction == 'U':
-            self.__pos = self.__pos[0] - 1, self.__pos[1]
-        elif direction == 'L':
-            self.__pos = self.__pos[0], self.__pos[1] - 1
-        elif direction == 'D':
-            self.__pos = self.__pos[0] + 1, self.__pos[1]
-        elif direction == 'R':
-            self.__pos = self.__pos[0], self.__pos[1] + 1
+    def step(self, y, x, jump_flag=0):
+        prev = self.__pos
+        self.__pos = self.__pos[0] + y, self.__pos[1] + x
         if self.tail:
-            self.__update_tail()
+            self.__update_tail(y, x, prev, jump_flag)
 
-    def __update_tail(self):
-        if self.__pos[0] - self.tail[0] > 1:
-            if not self.__straighten_tail('x'):
-                self.tail.step('D')
-
-        elif self.tail[0] - self.__pos[0] > 1:
-            if not self.__straighten_tail('x'):
-                self.tail.step('U')
-
-        elif self.__pos[1] - self.tail[1] > 1:
-            if not self.__straighten_tail('y'):
-                self.tail.step('R')
-
-        elif self.tail[1] - self.__pos[1] > 1:
-            if not self.__straighten_tail('y'):
-                self.tail.step('L')
+    def __update_tail(self, y, x, prev, jump_flag=0):
+        distance_y, distance_x = self.__distance()
+        if distance_y + distance_x > 2:
+            self.tail.step(y, x, 1)
+            new_y, new_x = prev[0] - self.tail.__pos[0], prev[1] - self.tail.__pos[1]
+            self.tail.step(new_y, new_x)
+        elif distance_y == 2 or distance_x == 2:
+            self.tail.step(y, x)
+        elif distance_y == distance_x == 1 and jump_flag:
+            self.tail.step(y, x, 1)
+        else:
+            pass
 
 
-#TODO: here is the error
-    # def __straighten_tail(self, axis):
-    #     if axis == 'x':
-    #         if self.tail[1] != self.__pos[1]:
-    #             self.tail.__pos = self.tail[0], self.__pos[1]
-    #     elif axis == 'y':
-    #         if self.tail[0] != self.__pos[0]:
-    #             self.tail.__pos = self.__pos[0], self.tail[1]
+    def __distance(self):
+        distance_y = abs(self.__pos[0] - self.tail[0])
+        distance_x = abs(self.__pos[1] - self.tail[1])
+        return distance_y, distance_x
 
     def __straighten_tail(self, axis):
-        if axis == 'x':
-            if self.tail[1] > self.__pos[1]:
-                self.tail.step('L')
-            elif self.tail[1] < self.__pos[1]:
-                self.tail.step('R')
-            else:
-                return False
-        elif axis == 'y':
-            if self.tail[0] > self.__pos[0]:
-                self.tail.step('U')
-            elif self.tail[0] < self.__pos[0]:
-                self.tail.step('D')
-            else:
-                return False
-        return True
+        pass
 
 
     def get_snake(self):
