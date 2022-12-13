a062e579cce0e271a874da6cfc3323ffef3b83b3
Maciej Ratajski <maciejratajski1999@gmail.com>
2022-12-13 16:03:51 +0100

    working solution for star 2

--- 9/main.py
+++ 9/main.py
@@ -5,7 +5,7 @@ directions = {'U': (-1, 0),
               'L': (0, -1),
               'R': (0, 1)}
 
-with open('input2.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     steps = []
     for line in file.readlines():
         line = line.strip()
@@ -24,22 +24,22 @@ with open('input2.txt', 'r') as file:
             for j in range(length-1):
                 tail = tail.tail
             visited.append((tail[0], tail[1]))
-            snake = rope.get_snake()
-            x_min, x_max = -0,5
-            x_range = x_max - x_min + 1
-            y_min, y_max = -5,0
-            y_range = y_max - y_min + 1
-            grid = [[str('.') for _j in range(x_range)] for _i in range(y_range)]
-            # print(grid)
-            # print(y_min, x_min)
-            for place in visited:
-            #     # print(place)
-                grid[place[0] - y_min][place[1] - x_min] = "#"
-            for i in range(len(snake)):
-                pos = snake[len(snake) - i - 1]
-                grid[pos[0] - y_min][pos[1] - x_min] = str(len(snake) - i - 1)
-            for row in grid:
-                print(''.join(row))
+            # snake = rope.get_snake()
+            # x_min, x_max = -2,7
+            # x_range = x_max - x_min + 1
+            # y_min, y_max = -7,2
+            # y_range = y_max - y_min + 1
+            # grid = [[str('.') for _j in range(x_range)] for _i in range(y_range)]
+            # # print(grid)
+            # # print(y_min, x_min)
+            # for place in visited:
+            # #     # print(place)
+            #     grid[place[0] - y_min][place[1] - x_min] = "#"
+            # for i in range(len(snake)):
+            #     pos = snake[len(snake) - i - 1]
+            #     grid[pos[0] - y_min][pos[1] - x_min] = str(len(snake) - i - 1)
+            # for row in grid:
+            #     print(''.join(row))
 
 print(len(set(visited)))
 visited = set(visited)
--- 9/node.py
+++ 9/node.py
@@ -2,6 +2,7 @@ class Node:
 
     def __init__(self, length=0):
         self.__pos = 0, 0
+        self.__length = length
         if length > 0:
             self.tail = Node(length-1)
         else:
@@ -17,34 +18,42 @@ class Node:
     def __getitem__(self, item):
         return self.pos()[item]
 
-    def step(self, y, x, jump_flag=0):
-        prev = self.__pos
+    def step(self, y, x):
         self.__pos = self.__pos[0] + y, self.__pos[1] + x
         if self.tail:
-            self.__update_tail(y, x, prev, jump_flag)
+            self.__update_tail()
 
-    def __update_tail(self, y, x, prev, jump_flag=0):
+    def __update_tail(self):
         distance_y, distance_x = self.__distance()
-        if distance_y + distance_x > 2:
-            self.tail.step(y, x, 1)
-            new_y, new_x = prev[0] - self.tail.__pos[0], prev[1] - self.tail.__pos[1]
-            self.tail.step(new_y, new_x)
-        elif distance_y == 2 or distance_x == 2:
-            self.tail.step(y, x)
-        elif distance_y == distance_x == 1 and jump_flag:
-            self.tail.step(y, x, 1)
-        else:
+        if distance_y == 1 and distance_x == 1:
             pass
-
+        elif distance_y + distance_x >= 2:
+            breakflag=0
+            for y in [-1, 0, 1]:
+                if breakflag:
+                    break
+                for x in [-1, 0, 1]:
+                    if x == 0 and y == 0:
+                        continue
+                    current_y, current_x = self.tail.__pos
+                    new_y, new_x = current_y + y, current_x + x
+                    if abs(self.__pos[0] - new_y) + abs(self.__pos[1] - new_x) == 1:
+                        self.tail.step(y, x)
+                        breakflag = 1
+                        break
+            else:
+                for direction in [(1, 1), (-1, 1), (-1, -1), (1, -1)]:
+                    y, x = direction
+                    current_y, current_x = self.tail.__pos
+                    new_y, new_x = current_y + y, current_x + x
+                    if abs(self.__pos[0] - new_y) == abs(self.__pos[1] - new_x) == 1:
+                        self.tail.step(y, x)
+                        break
 
     def __distance(self):
         distance_y = abs(self.__pos[0] - self.tail[0])
         distance_x = abs(self.__pos[1] - self.tail[1])
         return distance_y, distance_x
-
-    def __straighten_tail(self, axis):
-        pass
-
 
     def get_snake(self):
         pos = [self.__pos]
